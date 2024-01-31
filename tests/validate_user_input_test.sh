a192ce35b4609c37377da6de02d0f84a2eb0bd39
tommy-manjaro <153081087+tandc181123@users.noreply.github.com>
2024-01-31 15:37:07 +0800

    test: add forbidden character validation
    Update validate_user_input_test.sh to test for forbidden characters.
    The password regex now disallows [^[:space:]\\\/\%\=\'\"\<\>\,\*\?].
    Test cases cover various scenarios with forbidden chars.

--- tests/validate_user_input_test.sh
+++ tests/validate_user_input_test.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# sources
 script_dir=$(dirname "${BASH_SOURCE[0]}")
 . "$script_dir/../functions.sh"
 . "$script_dir/../regex_patterns.sh"
+
+# password_regex=^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^[:alnum:]_])$
+# password-length limitation {8,12}
 
 function provider_length_valid_data() {
   local items=("Aa!12345" "Aa!123456" "Aa!1234567" "Aa!12345678" "Aa!123456789")
@@ -23,6 +27,20 @@ function provider_length_invalid_data() {
 
 # data_provider provider_length_invalid_data
 function test_data_length_invalidation() {
+  local item=$1
+  result=$(validate_user_input "$item" "$password_regex")
+  assert_equals "1" "$result"
+}
+
+# password-forbidden characters [^[:space:]\\\/\%\=\'\"\<\>\,\*\?]
+
+function provider_forbidden_data() {
+  local items=("Ac!1234 " "Ac!1234\\" "Ac!1234/" "Ac!1234%" "Ac!1234=" "Ac!1234'" "Ac!1234\"" "Ac!1234<" "Ac!1234>" "Ac!1234," "Ac!1234*" "Ac!1234?")
+  echo "${items[@]}"
+}
+
+# data_provider provider_forbidden_data
+function test_contains_forbiden_chars() {
   local item=$1
   result=$(validate_user_input "$item" "$password_regex")
   assert_equals "1" "$result"
